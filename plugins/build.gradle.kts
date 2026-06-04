d674736970737f98196ff4581f18316c14eacd56
Skye Elliot <actuallyori@gmail.com>
2026-06-04 11:14:03 +0100

    chore: Remove unused Firebase distributio; Sonar; release tooling

--- plugins/build.gradle.kts
+++ plugins/build.gradle.kts
@@ -19,8 +19,6 @@ dependencies {
     implementation(libs.android.gradle.plugin)
     implementation(libs.kotlin.gradle.plugin)
     implementation(libs.kover.gradle.plugin)
-    implementation(platform(libs.google.firebase.bom))
-    implementation(libs.firebase.appdistribution.gradle)
     implementation(files(libs.javaClass.superclass.protectionDomain.codeSource.location))
     implementation(libs.autonomousapps.dependencyanalysis.plugin)
     implementation(libs.metro.gradle.plugin)
